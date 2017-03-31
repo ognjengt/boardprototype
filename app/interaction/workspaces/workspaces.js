ab8f771ce3ce2f56d31af254de062406e3b8cd1c
Ognjen Gatalo <ognjeng95@gmail.com>
2017-03-31 14:04:57 +0200

    Paljenje addWorkspace modela radi, skripta workspaces.js uspesno loadovana na strani.

--- app/interaction/workspaces/workspaces.js
+++ app/interaction/workspaces/workspaces.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-  console.log('Hello workspaces');
+   var $btnAddNewWorkspace = $('#btnAddNewWorkspace');
+   var $addPopupEdit = $('#addPopupEdit');
+   var $workspacePageContent = $('#workspacePageContent');
+  
+   $btnAddNewWorkspace.on('click',function() {
+      openNewWorkspacePopup();
+   })
+
+  // //Functions
+  function openNewWorkspacePopup() {
+    $addPopupEdit.velocity("fadeIn");
+    $workspacePageContent.hide();
+  }
 });
