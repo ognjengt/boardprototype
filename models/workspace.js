ee8fa3d44fd18fa1795c7c8f0cd20f1510ce2f74
Ognjen Gatalo <ognjeng95@gmail.com>
2017-04-18 20:00:11 +0200

    Namestio da ne mogu duplikati boardova da se dodaju u isti workspace
    TODO/NOTE: Vratiti korisniku informaciju da taj board vec postoji u
    nekim workspaceovima.
    
    Nesto zeza nekad ostaje samo Populating... to pogledati sta je,
    verovatno ce sve biti ok na heroku-u

--- models/workspace.js
+++ models/workspace.js
@@ -49,16 +49,24 @@ module.exports.populateWithBoard = function(boardId,workspaces,res) {
   var i = 0;
   workspaces.forEach(function(workspaceId) {
     // proveriti da se doda novi samo ukoliko on vec ne postoji u tom workspaceu
-    Workspace.findByIdAndUpdate(workspaceId, {$push: {boards: new Object({_id: boardId})}},{new: true}, function(err) {
-    if (err) {
-      throw err;
-    }
+
+    Workspace.findById(workspaceId,function(err,ws) {
+      console.log(ws.boards.indexOf(boardId));
+      if(!(ws.boards.indexOf(boardId) > -1)) {
+        Workspace.findByIdAndUpdate(workspaceId, {$push: {boards: new Object({_id: boardId})}},{new: true}, function(err) {
+          if (err) {
+            throw err;
+          }
+          });
+      }
+    });
+
       i++;
       if(i == workspaces.length) {
         res.send(workspaces);
         res.end();
       }
-    });
+    
   });
   // Ovo isto radi ali kaze cant set headers after they are sent, sto znaci da on vrati res send odma, a ovi to i ne izvrse, tako da ovo iznad je bukvalno isto resenje sa workaroundom
   
